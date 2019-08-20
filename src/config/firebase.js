1f2883cd4e6d6e2d727ad06949aecc56bad3a290
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-08-20 10:28:44 +0300

    added a 'config' folder with a 'firebase.js' file that includes all the configuration of firebase for our app

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyCMDzCons4EZ1clV02C6HI6HsZL1XJ-Md4",
+    authDomain: "smallbiz-6c996.firebaseapp.com",
+    databaseURL: "https://smallbiz-6c996.firebaseio.com",
+    projectId: "smallbiz-6c996",
+    storageBucket: "",
+    messagingSenderId: "709512455520",
+    appId: "1:709512455520:web:ae4170e26d2b568e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export default firebase
