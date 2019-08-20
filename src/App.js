28a246b14fb891dd4219e58fe26022dedfd8bf2f
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-08-20 10:49:59 +0300

    added a function to check authentication by the user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
+import firebase from './config/firebase'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      user: {},
 
     }
+  }
+  componentDidMount() {
+      this.authListener()
+  }
+  authListener() {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user)
+      if (user) {
+        this.setState({ user })
+
+      } else {
+        this.setState({ user: null })
+
+      }
+    })
   }
   render() {
     return (
       <Router>
         <nav>
           <div class="nav-wrapper navBar #212121 grey darken-4">
+            {this.state.user}
             <a href="" class="brand-logo right">smallBiz</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
               <li ><Link to="/singup">singup  </Link></li>
