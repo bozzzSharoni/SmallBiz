449d06811535530b1ac3c6190f35f8cabf906bb6
mor bargig <morbargig@gmail.com>
2019-08-25 11:25:04 +0300

    for gily

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AppointmentSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User' },
-    businessId: { type: Schema.Types.ObjectId, ref: 'Business' },
+    userId: String,
+    businessId: String,
+    // userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    // businessId: { type: Schema.Types.ObjectId, ref: 'Business' },
     date: String,
     time: String,
     rating: Number,
