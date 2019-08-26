9fa488eea2616490017ddcdf92c604e03a3b743f
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-08-26 20:21:58 +0300

    ahla mor ve ahla gili

--- src/component/Home.js
+++ src/component/Home.js
@@ -155,7 +155,7 @@ class Home extends Component {
 
                 <input name="input" type="text" value={this.state.fullName} onChange={this.updateusersText} placeholder="type here" /></button>
             <h4> {this.state.loggedInUserName !== undefined ? "welcome back " + this.state.loggedInUserName : null} </h4>
-            <img className="circle responsive-img" src={this.state.loggedInUserImg} />
+            
             <img width="200" height="200" className="circle responsive-img" src={this.state.loggedInUserImg} />
             <div className="categories">
                 {this.props.Catgories !== undefined ? this.props.Catgories.map(c =>
